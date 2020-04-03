1e0f118cc344f65b5d26a949228b355e3831d0e6
Yao Jiang <15611775219@163.com>
2020-04-03 18:53:31 +0800

    update2 trial.py

--- trial.py
+++ trial.py
@@ -26,3 +26,32 @@ rects2=plt.bar(ind+width+adjust,girls,width,color='salmon',edgecolor='navy',line
 mpl.rcParams['font.sans-serif']=['SimHei']
 
 #使matplotlib显示中文字体
+
+
+plt.title('北航2016级新生男女比')
+plt.xlabel('院系')
+plt.ylabel('人数')
+
+#设置标题及xy轴标签
+
+plt.xticks(ind+width+adjust)
+
+#设置x轴记号的位置
+
+plt.legend((rects1,rects2),('boys','girls'))
+
+#设置图例
+
+ax.set_xticklabels(('知行书院','飞行学院','高等理工学院'))
+
+#将x轴记号设置为具体文字
+
+def autolabel(rects):
+    for rect in rects:
+        height=rect.get_height()
+        ax.text(rect.get_x()+rect.get_width()/2,1.005*height,int(height),ha='center',va='bottom',fontsize=12,withdash=False)
+
+#定义函数以得到柱高并显示在适当位置
+        
+autolabel(rects1)
+autolabel(rects2)
